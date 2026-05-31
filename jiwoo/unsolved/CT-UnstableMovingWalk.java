8240066254f9cf2380fa0d62449d7537132bca9e
jiwoo <cgu99@naver.com>
2026-06-01 03:19:12 +0900

    [unsolved][CT-UnstableMovingWalk] 불안한 무빙워크 - 최지우

---
+++ jiwoo/unsolved/CT-UnstableMovingWalk.java
@@ -0,0 +1,100 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.ArrayDeque;
+import java.util.Queue;
+import java.util.StringTokenizer;
+
+/*
+ * 	시뮬레이션 (76분)
+ * 
+ * 	칸: 고정된 위치
+ * 	판: 초기의 칸의 번호, 회전할 때마다 이동
+ */
+public class Main_불안한무빙워크 {
+
+	static int n;  // 무빙워크의 길이
+	static int k;  // 실험 종료 기준이 되는 안정성이 0개인 판의 개수
+	static int[] stability;  // 각 판의 안정성
+	static int cnt;  // 안정성이 0인 판의 개수
+	static int cur1;  // 현재 1번 칸에 위치한 판의 번호
+	static Queue<Integer> queue;  // 사람이 있는 판 (가장 먼저 올라간 사람부터 이동 -> FIFO -> Queue)
+	static int people;  // 현재 무빙워크에 있는 사람 수
+	static boolean[] used;  // 각 판에 사람이 있는지 (사람이 올라갈 수 있는 칸은 1~n번 칸)
+	
+	public static void main(String[] args) throws IOException {
+		
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st = new StringTokenizer(br.readLine());
+		
+		n = Integer.parseInt(st.nextToken());
+		k = Integer.parseInt(st.nextToken());
+		
+		stability = new int[2*n];
+		cnt = 0;
+		st = new StringTokenizer(br.readLine());
+		for(int i = 0; i < 2 * n; i++) {
+			stability[i] = Integer.parseInt(st.nextToken());
+			if(stability[i] == 0) cnt++;
+		}
+		
+		/* 입력 종료 */
+		
+		cur1 = 0;
+		int result = 0;  // 현재 진행 중인 실험이 몇 번째 실험인지
+		queue = new ArrayDeque<>();
+		people = 0;
+		used = new boolean[2*n];
+		
+		while (cnt < k) {
+			
+			result++;
+			System.out.println(result + "번째 실험");
+			
+			// 1. 무빙워크 한 칸 회전
+			cur1 = (2*n + cur1-1) % (2*n);
+			
+			// 2. 사람 이동
+			if(!queue.isEmpty()) move(people);
+			
+			// 3. 1번 칸에 사람 올리기
+			if(!used[cur1] && stability[cur1] != 0) {
+				queue.add(cur1);
+				used[cur1] = true;
+				people++;
+			}
+			System.out.println("사람 수: " + people);
+			
+		}
+		
+		System.out.println(result);
+		
+	}
+	
+	private static void move(int p) {
+		
+		while(p-- > 0) {  // 무빙워크에 있는 사람 수만큼 반복
+			
+			int cur = queue.poll();  // 사람이 있는 판의 번호		
+			int next = (cur + 1) % (2 * n);  // 앞선 칸의 판의 번호
+			
+			// 이동 불가능: 앞선 칸에 사람이 이미 있거나 앞선 칸의 안정성이 0인 경우
+			if(used[next] || stability[next] == 0) {
+				queue.add(cur);
+				continue;
+			}
+			
+			// 이동 가능
+			used[cur] = false;
+			if(next == cur1 + n-1) people--;
+			else {
+				queue.add(next);
+				used[next] = true;
+				if(--stability[next] == 0) cnt++;
+				System.out.println("안정성이 0인 판의 수: " + cnt);
+			}
+			
+		}
+	}
+
+}
