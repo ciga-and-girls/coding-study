624b973e07a80cb88bd00a43e974a224ce23b2f4
kyungrin Kim <0123jade0123@gmail.com>
2026-06-03 17:20:00 +0900

    [solved] [PGM-150368] 이모티콘 할인행사 - 김경린

--- kyungrin/solved/PGM_150368.java
+++ kyungrin/solved/PGM_150368.java
@@ -1,6 +1,4 @@
-package kyungrin.unsolved;
-
-import java.util.ArrayList;
+package kyungrin.solved;
 
 /** 행사 목적을 최대한으로 달성 이모티콘 플러스 서비스 가입 수, 이모티콘 매출액
  * 1. 이모티콘 플러스 서비스 가입자를 최대한 늘리는 것.
@@ -23,8 +21,7 @@ import java.util.ArrayList;
  * 1. 각 이모티콘에 대해 할인률을 적용하기
  *   - 모든 경우의 수
  *   - 중복 할인 가능
- *   - 순서 상관 없음
- *   => 중복 조합 7H4 = 210
+ *   - 아... 조합이 아니라 순열이다... 1번 이모티콘에 적용하는 거랑 2번 이모티콘에 적용하는 거랑 완전 다르니까
  *   : 각 이모티콘의 할인률을 결정할 때마다 연쇄적으로 결정되어야 하는 것
  *    - 사용자가 해당 이모티콘을 구매하는가? (해당 할인률을 사용자가 수용하는가)
  *
@@ -36,7 +33,6 @@ public class PGM_150368 {
   static int emtCnt;
   static int userCnt;
   static int[] emoticonsSale; // temp, idx: 이모티콘 번호, value : 이모티콘 할인률(1~4)
-  static ArrayList[] usersBought; // temp, idx: 유저 번호, value: 유저가 구매한 이모티콘 번호
   //
   static int maxCnt;
   static int maxSale;
@@ -46,7 +42,10 @@ public class PGM_150368 {
     emtCnt = EMOTICONS.length;
     userCnt = USERS.length;
 
-    dc(0, 10);
+    emoticonsSale = new int[emtCnt];
+    maxCnt = Integer.MIN_VALUE;
+    maxSale = Integer.MIN_VALUE;
+    dc(0);
 
     return new int[]{maxCnt, maxSale};
   }
@@ -55,39 +54,48 @@ public class PGM_150368 {
   // 매개변수
   // idx: 이모티콘 번호
   // start: 할인률의 시작 번호 (1~4)
-  private static void dc(int idx, int start) {
+  private static void dc(int idx) {
     if(idx == emtCnt){
-
-      int plus = 0;
+      int totalPlus = 0;
       int totalCost = 0;
-      // 1. 유저 별 구매 비용 확인
 
-      // 1-1. 각 유저 중 구매 비용이 유저의 기준을 넘으면 -> 이모티콘 플러스 ++
+      // 1. 선택된 할인률이 사용자의 할인률 이상인지 확인
+      // 10%, 20%, 30%, 40%
+      // 1. 유저 별 구매 비용 확인
+      for(int i = 0; i < userCnt; i++) {
+        int[] info = USERS[i];
+        int cost = 0;
+
+        for(int e = 0; e < emtCnt; e++){
+          int eRatio = emoticonsSale[e];
+          int pRatio = info[0];
+          if (eRatio >= pRatio) {
+            cost += ((100 - eRatio) * EMOTICONS[e] / 100);
+          }
+        }
+        // 1-1. 각 유저 중 구매 비용이 유저의 기준을 넘으면 -> 이모티콘 플러스 ++
+        if (cost >= info[1]) totalPlus++;
+        else totalCost += cost;
+      }
 
       // 2. plus 갱신
-      // 2-2. plus가 같다면 totalCost 갱신
+      if (maxCnt < totalPlus) {
+        maxCnt = totalPlus;
+        maxSale = totalCost;
+      }
+      else if (maxCnt == totalPlus){
+        // 2-2. plus가 같다면 totalCost 갱신
+        maxSale = Math.max(maxSale, totalCost);
+      }
 
       return;
     }
 
     //
-    for(int i = start; i < 40; i += 10){
+    for(int i = 10; i <= 40; i += 10){
       // 0. emoticonsSale 에 선택된 할인률 부여
       emoticonsSale[idx] = i;
-
-      // 1. 선택된 할인률이 사용자의 할인률 이상인지 확인
-      // 10%, 20%, 30%, 40%
-      for(int j = 0; j < userCnt; j++) {
-        int[] info = USERS[j];
-
-        int ratio = info[0];
-        if (i >= ratio) {
-          // 2. 이상이라면 usersBought에 이모티콘 번호(idx) 추가
-          usersBought[j].add(idx); // 어 이러면 데이터 더러워질 것 같은데
-        }
-      }
-
-      dc(idx + 1, i);
+      dc(idx + 1);
     }
   }
 }
